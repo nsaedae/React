2ca1332f93efa90b900b755f1c2df45199792e35
unknown <nsaedae@gmail.com>
2022-05-10 12:11:09 +0900

    2022/05/10 수업예제

---
+++ ch03/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import MyClassComponent from './components/MyClassComponent';
+import MyFuncComponent from './components/MyFuncComponent';
+
+/*
+  날짜 : 2022/05/10
+  이름 : 김철학
+  내용 : React 컴포넌트 실습하기
+
+  컴포넌트(Component)
+   - UI 또는 기능을 독립적으로 나뉜 모듈형태
+   - 컴포넌트는 클래스형과 함수형으로 나뉨
+*/
+function App() {
+  return (
+    <div>
+      <h3>ch03.React 컴포넌트 실습</h3>
+
+      <h4>클래스형 컴포넌트</h4>
+      <MyClassComponent/>
+
+      <h4>함수형 컴포넌트</h4>
+      <MyFuncComponent/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ch03/src/components/MyFuncComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './MyFuncComponent.css';
+
+const MyFuncComponent = function() {
+
+    return (
+        <div className="MyFuncComponent">
+            <h5>MyFuncComponent</h5>
+            <p>
+                MyFuncComponent에서 출력
+            </p>
+        </div>
+    );
+}
+
+export default MyFuncComponent;
